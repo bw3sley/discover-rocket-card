7b3bb3fe40893ba32d481fc35ad75980a3ebc557
Wesley Bernardes <bw3sley@gmail.com>
2022-05-05 22:05:05 -0300

    :sparkles: feat: create fetch functionality to receive data from GitHub profile

--- main.js
+++ main.js
@@ -18,6 +18,103 @@ function ChangeBorderColor() {
     const card = document.querySelector("div#card").style.borderColor = borderColor;
 }
 
+const url = "https://api.github.com/users/bw3sley";
+const token = "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";
+
+function GetUserLocation() {
+    axios.get(url, {
+        headers: {
+            'Authorization': `token ${token}`
+        }
+    })
+    .then(response => {
+        const data = response.data.location;
+
+        const location = data.substring(0, 7);
+
+        const userLocation = document.querySelector("p#location").textContent = location;
+    })
+    .catch(error => alert(`Error: ${error}`))
+}
+
+GetUserLocation();
+
+function GetUserCompany() {
+    axios.get(url, {
+        headers: {
+            "Authorization": `token ${token}`
+        }
+    })
+    .then(response => {
+        const data = response.data.company;
+        const userCompany = document.querySelector("#company").textContent = `@${data}`;
+    })
+    .catch(error => alert(`Error: ${error}`))
+}
+
+GetUserCompany();
+
+function GetUserLogin() {
+    axios.get(url, {
+        headers: {
+            "Authorization": `token ${token}`
+        }
+    })
+    .then(response => {
+        const data = response.data.login;
+        const userLogin = document.querySelector("a#user-github").textContent = data;
+    })
+    .catch(error => alert(`Error: ${error}`))
+}
+
+GetUserLogin();
+
+function GetUserFollowers() {
+    axios.get(url, {
+        headers: {
+            "Authorization": `token ${token}`
+        }
+    })
+    .then(response => {
+        const data = response.data.followers;
+        const userFollowers = document.querySelector("p#followers").textContent = `${data} Seguidores`;
+    })
+    .catch(error => alert(`Error: ${error}`))
+}
+
+GetUserFollowers();
+
+function GetUserFollowers() {
+    axios.get(url, {
+        headers: {
+            "Authorization": `token ${token}`
+        }
+    })
+    .then(response => {
+        const data = response.data.following;
+        const userFollowers = document.querySelector("p#following").textContent = `${data} Seguindo`;
+    })
+    .catch(error => alert(`Error: ${error}`))
+}
+
+GetUserFollowers();
+
+function GetUserRepos() {
+    axios.get(url, {
+        headers: {
+            "Authorization": `token ${token}`
+        }
+    })
+
+    .then(response => {
+        const data = response.data.public_repos;
+        const userRepos = document.querySelector("p#repos").textContent = `${data} Repositórios`;
+    })
+    .catch(error => alert(`Error: ${error}`))
+}
+
+GetUserRepos();
+
 const scrollReveal = ScrollReveal({
     origin: "top",
     distance: "32px",
